26aa0c8f18f99ca720ee491b8ea84e00bcb4c9cf
Aleksey Filippov <sarum9in@gmail.com>
2013-02-24 22:37:50 +0400

    easy::escaping test set.

---
+++ tests/easy.cpp
@@ -0,0 +1,31 @@
+#define BOOST_TEST_MODULE easy
+#include <boost/test/unit_test.hpp>
+
+#include "bunsan/curl/easy.hpp"
+
+BOOST_AUTO_TEST_SUITE(easy)
+
+BOOST_AUTO_TEST_CASE(escaping)
+{
+    static const std::vector<std::pair<std::string, std::string>> tests = {
+        {"", ""},
+        {"123", "123"},
+        {"string with spaces", "string%20with%20spaces"},
+        {"multiline\nstring", "multiline%0Astring"},
+        {std::string("string\0with\0zeroes", 18), "string%00with%00zeroes"},
+    };
+
+    bunsan::curl::easy curl;
+
+    for (const std::pair<std::string, std::string> &test: tests)
+    {
+        const std::string &unescaped = test.first;
+        const std::string &escaped = test.second;
+        const std::string escaped_ = curl.escape(unescaped);
+        const std::string unescaped_ = curl.unescape(escaped_);
+        BOOST_CHECK_EQUAL(escaped_, escaped);
+        BOOST_CHECK_EQUAL(unescaped_, unescaped);
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END() // easy
