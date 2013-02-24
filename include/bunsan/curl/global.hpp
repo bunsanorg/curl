6da05101aeba8ea320c0420aefdc31064b5c7e41
Aleksey Filippov <sarum9in@gmail.com>
2013-02-24 21:51:19 +0400

    global implementation.

---
+++ include/bunsan/curl/global.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <curl/curl.h>
+
+#include <boost/noncopyable.hpp>
+
+namespace bunsan{namespace curl
+{
+    /// \warning This class is not thread-safe.
+    class global: private boost::noncopyable
+    {
+    public:
+        explicit global(const long flags=CURL_GLOBAL_DEFAULT);
+
+        global(const long flags,
+               const curl_malloc_callback m,
+               const curl_free_callback f,
+               const curl_realloc_callback r,
+               const curl_strdup_callback s,
+               const curl_calloc_callback c);
+
+        ~global();
+
+    private:
+        /*!
+         * Library should be initialized by this object.
+         *
+         * \warning Currently there is no way to initialize library with non-default constructor.
+         *
+         * \todo Non-default library initialization.
+         */
+        static const global static_instance;
+    };
+}}
---
+++ src/global.cpp
@@ -0,0 +1,31 @@
+#include "bunsan/curl/global.hpp"
+#include "bunsan/curl/error.hpp"
+
+namespace bunsan{namespace curl
+{
+    global::global(const long flags)
+    {
+        const CURLcode ret = ::curl_global_init(flags);
+        if (ret)
+            BOOST_THROW_EXCEPTION(easy_error(ret));
+    }
+
+    global::global(const long flags,
+                   const curl_malloc_callback m,
+                   const curl_free_callback f,
+                   const curl_realloc_callback r,
+                   const curl_strdup_callback s,
+                   const curl_calloc_callback c)
+    {
+        const CURLcode ret = ::curl_global_init_mem(flags, m, f, r, s, c);
+        if (ret)
+            BOOST_THROW_EXCEPTION(easy_error(ret));
+    }
+
+    global::~global()
+    {
+        ::curl_global_cleanup();
+    }
+
+    const global global::static_instance;
+}}
