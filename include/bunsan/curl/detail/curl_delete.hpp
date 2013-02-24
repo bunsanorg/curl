c788de918a3ef2de7162084664a0cf78f5a758cc
Aleksey Filippov <sarum9in@gmail.com>
2013-02-24 22:17:46 +0400

    detail::curl_delete implementation.

---
+++ include/bunsan/curl/detail/curl_delete.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace bunsan{namespace curl{namespace detail
+{
+    struct curl_delete
+    {
+        constexpr curl_delete() noexcept=default;
+
+        void operator()(void *const ptr) const;
+    };
+}}}
---
+++ src/detail/curl_delete.cpp
@@ -0,0 +1,8 @@
+#include "bunsan/curl/detail/curl_delete.hpp"
+
+#include <curl/curl.h>
+
+void bunsan::curl::detail::curl_delete::operator()(void *const ptr) const
+{
+    ::curl_free(ptr);
+}
