7ac18260a128ce8d352f587c6557e5cb7dcbc07a
Aleksey Filippov <sarum9in@gmail.com>
2013-02-24 17:44:41 +0400

    easy_category and multi_category implementation.

---
+++ include/bunsan/curl/error_category.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <system_error>
+
+namespace bunsan{namespace curl
+{
+    const std::error_category &easy_category() noexcept;
+    const std::error_category &multi_category() noexcept;
+}}
---
+++ src/error_category.cpp
@@ -0,0 +1,49 @@
+#include "bunsan/curl/error_category.hpp"
+
+#include <curl/curl.h>
+
+namespace bunsan{namespace curl
+{
+    namespace
+    {
+        class easy_category_: public std::error_category
+        {
+        public:
+            const char *name() const noexcept override
+            {
+                return "curl_easy";
+            }
+
+            std::string message(int code) const override
+            {
+                return ::curl_easy_strerror(static_cast<CURLcode>(code));
+            }
+        };
+
+        class multi_category_: public std::error_category
+        {
+        public:
+            const char *name() const noexcept override
+            {
+                return "curl_multi";
+            }
+
+            std::string message(int code) const override
+            {
+                return ::curl_multi_strerror(static_cast<CURLMcode>(code));
+            }
+        };
+    }
+
+    const std::error_category &easy_category() noexcept
+    {
+        static const easy_category_ impl;
+        return impl;
+    }
+
+    const std::error_category &multi_category() noexcept
+    {
+        static const multi_category_ impl;
+        return impl;
+    }
+}}
